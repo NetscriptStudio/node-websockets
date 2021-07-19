f71cbbe0dd7d72dd6baa7368e9a50d251fb493f2
NetscriptStudio <83537037+NetscriptStudio@users.noreply.github.com>
2021-07-19 13:30:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -17,13 +17,16 @@ var data = {}
 wss.on('connection', (ws) => {
 	console.log('Client connected');
 
+  ws.client.send(JSON.stringify(data));
+
 	ws.on('message', (msg) => {
 		console.log(JSON.parse(msg));
-    		var mess = JSON.parse(msg);
+    var mess = JSON.parse(msg);
 
-    		data[mess.name] = mess
-    		wss.clients.forEach((client) => {
-      		client.send(JSON.stringify(data));
-    		})
-	});
+    data[mess.gameName][mess.name] = mess
+    wss.clients.forEach((client) => {
+
+      client.send(JSON.stringify(data));
+    	})
+	  });
 });
